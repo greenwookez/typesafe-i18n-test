6a91a516b6556e2b12576ae36b699ce3a6a0fba7
Bogdan Bogdanov <greenwookez@gmail.com>
2022-06-12 21:31:13 +0300

    cleanup

---
+++ src/i18n/de/namespace/index.ts
@@ -0,0 +1,8 @@
+import type { NamespaceNamespaceTranslation } from '../../i18n-types'
+
+const de_namespace: NamespaceNamespaceTranslation = {
+	// TODO: insert translations
+
+}
+
+export default de_namespace
---
+++ src/i18n/en/namespace/index.ts
@@ -0,0 +1,5 @@
+import type { BaseTranslation } from '../../i18n-types'
+
+const en_namespace: BaseTranslation = { }
+
+export default en_namespace
